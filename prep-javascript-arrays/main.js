a68bec0a493a33fdbf715d8ef1a36dc29c39d0a1
Janice Lee <meekukin@gmail.com>
2022-01-16 13:31:53 -0800

    Completed prep-javascript-arrays exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,6 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+console.log('The first element of the array is ' + languages[0]);
+console.log('The 3rd element of the array is ' + languages[2]);
+console.log('The length of the array is ' + languages.length);
+console.log('The last element of the array is ' + languages[5]);
