5fb60fcb6ddbcfb8f6f3833eeaf3d691eed39a1b
Janice Lee <meekukin@gmail.com>
2022-01-17 13:53:45 -0800

    Modified main.js

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -3,4 +3,4 @@ console.log(languages);
 console.log('The first element of the array is ' + languages[0]);
 console.log('The 3rd element of the array is ' + languages[2]);
 console.log('The length of the array is ' + languages.length);
-console.log('The last element of the array is ' + languages[5]);
+console.log('The last element of the array is ' + languages[languages.length - 1]);
